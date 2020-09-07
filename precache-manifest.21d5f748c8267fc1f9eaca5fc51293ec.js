2cfcb66e12372b16228fa855c162c985c0c9feb3
Mico Kontic <micokontic@gmail.com>
2020-09-07 14:05:27 +0200

    Updates

---
+++ precache-manifest.21d5f748c8267fc1f9eaca5fc51293ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b9a388c6622cf95c9cdf01d8187159",
+    "url": "/to_do/index.html"
+  },
+  {
+    "revision": "9d1c0c44e10c32ef5d53",
+    "url": "/to_do/static/css/main.fe25df7e.chunk.css"
+  },
+  {
+    "revision": "1afc8b26e5c07659eadd",
+    "url": "/to_do/static/js/2.c19ed6c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do/static/js/2.c19ed6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1c0c44e10c32ef5d53",
+    "url": "/to_do/static/js/main.60c8d67c.chunk.js"
+  },
+  {
+    "revision": "5b6fe0a39d95288cbfe4",
+    "url": "/to_do/static/js/runtime-main.8663b774.js"
+  }
+]);
